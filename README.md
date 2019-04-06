47dfcf88b59d6e58239e6a024c9042e122d927e1
James Millner <jamesmillnerdev@gmail.com>
2019-04-07 00:17:58 +0100

    Added readme, slight refactoring.

---
+++ README.md
@@ -0,0 +1,15 @@
+### Kotlin Strava Spring Boot Application
+A simple Kotlin Spring Boot app designed for handling GPX files downloaded from Strava.
+
+The app currently provides a single endpoint HTTP endpoint to translate a GPX file into a provided output:
+- POST: `/strava/file-upload?outputType=json`
+
+| Params         | Supported Values    | 
+| -------------  |:-------------:      | 
+| outputType     | json,xml,dataobject | 
+
+
+#### Example Request
+`
+curl -F 'file=@<localfile>.gpx' http://localhost:8080/strava/file-upload
+`
--- src/main/kotlin/strava/controller/StravaApplicationController.kt
+++ src/main/kotlin/strava/controller/StravaApplicationController.kt
@@ -1,14 +1,11 @@
 package strava.controller
 
 import org.apache.commons.io.IOUtils
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import strava.gpx.GPXReader
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
-import org.springframework.web.multipart.MultipartFile
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import strava.gpx.GPXReader
 import strava.gpx.createGpxDataObjectFromXML
 import java.nio.charset.StandardCharsets
 
@@ -18,8 +15,7 @@ class StravaApplicationController(var gpxReader: GPXReader) {
 
     @PostMapping("/strava/file-upload")
     fun handleFileUpload(@RequestParam("file") file: MultipartFile,
-                         @RequestParam("outputType") outputType: String,
-                         redirectAttributes: RedirectAttributes): String {
+                         @RequestParam("outputType") outputType: String): String {
 
         val xmlAsString = IOUtils.toString(file.inputStream, StandardCharsets.UTF_8.name())
 
@@ -27,7 +23,7 @@ class StravaApplicationController(var gpxReader: GPXReader) {
             "json" -> gpxReader.readFileToJson(xmlAsString)
             "xml" -> xmlAsString
             "dataobject" -> createGpxDataObjectFromXML(xmlAsString).toString()
-            else -> throw Exception("Unsupported output type. Please select from `json`, `xml`, `dataobject` (Produces kotlin data object.toString())" )
+            else -> throw Exception("Unsupported output type. Please select from `json`, `xml`, `dataobject` (Produces kotlin data object.toString())")
         }
     }
 
