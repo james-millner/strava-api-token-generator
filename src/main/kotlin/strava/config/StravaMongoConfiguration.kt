e0809df3539922fb3d44e59ab1fedf32a95dd838
James Millner <jamesmillnerdev@gmail.com>
2019-05-25 00:08:51 +0100

    Added docker profile.

--- src/main/kotlin/strava/config/StravaMongoConfiguration.kt
+++ src/main/kotlin/strava/config/StravaMongoConfiguration.kt
@@ -13,11 +13,13 @@ import org.springframework.data.mongodb.core.mapping.MongoMappingContext
 
 
 @Configuration
-class SpringMongoConfig(@Value("\${spring.data.mongodb.database}") val databaseName: String) {
+class SpringMongoConfig(@Value("\${spring.data.mongodb.database}") val databaseName: String,
+                        @Value("\${spring.data.mongodb.host}") val mongoHost: String,
+                        @Value("\${spring.data.mongodb.port}") val mongoPort: Int) {
 
     @Bean
     fun mongoDbFactory(): MongoDbFactory {
-        return SimpleMongoDbFactory(MongoClient(), databaseName)
+        return SimpleMongoDbFactory(MongoClient(mongoHost, mongoPort), databaseName)
     }
 
     @Bean
