9f031d6848b176bdeea058bce53fbfbb0cffe3d3
James Millner <jamesmillnerdev@gmail.com>
2019-07-28 16:12:21 +0100

    Swapped auto-generated Number class for Int

--- src/main/kotlin/strava/gpx/models/GPXDataClasses.kt
+++ src/main/kotlin/strava/gpx/models/GPXDataClasses.kt
@@ -4,14 +4,14 @@ data class GpxObject(val gpx: Gpx?)
 
 data class Extensions(val trackpointextension: Trackpointextension?)
 
-data class Gpx(val creator: String?, val xmlns: String?, val metadata: Metadata?, val gpxtpx: String?, val schemalocation: String?, val xsi: String?, val gpxx: String?, val trk: Trk?, val version: Number?)
+data class Gpx(val creator: String?, val xmlns: String?, val metadata: Metadata?, val gpxtpx: String?, val schemalocation: String?, val xsi: String?, val gpxx: String?, val trk: Trk?, val version: Int?)
 
 data class Metadata(val time: String?)
 
 data class Trackpointextension(val hr: Int?, val cad: Int?, val atemp: Int?)
 
-data class Trk(val trkseg: Trkseg?, val name: String?, val type: Number?)
+data class Trk(val trkseg: Trkseg?, val name: String?, val type: Int?)
 
-data class Trkpt(val extensions: Extensions?, val lon: Number?, val time: String?, val lat: Number?, val ele: Number?)
+data class Trkpt(val extensions: Extensions?, val lon: Int?, val time: String?, val lat: Int?, val ele: Int?)
 
 data class Trkseg(val trkpt: List<Trkpt>?)
